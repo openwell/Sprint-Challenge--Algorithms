fb4572b8bd8d40711090b6b4ee17c5030b7d07ec
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-10-11 10:39:26 +0100

    attempted count with all test passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,20 @@
 '''
-Your function should take in a signle parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should take in a single parameter (a string `word`)
+Your function should return a count of how many occurrences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
+
+    n=0
+    if len(word) < 2:
+        return 0
+
+    count = 1 if ((word[n] + word[n+1]) == 'th') else 0
+    return count + count_th(word[1:])
     # TBC
     
-    pass
+    # pass
+print(count_th('tiiththth'))
+
+
+
